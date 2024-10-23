e2bfba1f70babfadd49440c7e619e362026e1efe
cuogrec <cuong.np233293@sis.hust.edu.vn>
2024-10-23 20:51:08 +0700

    Create baitap23-10-2024.cpp

---
+++ baitap23-10-2024.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+  int dem[100];
+	int main(){
+		int n ; cin >> n;
+		int max = -1 , min = 1e7;
+		int a[n+1];
+		for(int i = 0; i<=n ; i++){
+			cin >> a[i];
+			dem[a[i]]++;
+			if(a[i] > max){
+				max = a[i];
+			}
+			if(a[i] < min){
+				min = a[i];
+			}
+		}
+		cout << "SO NGUOI SINH RA NHIEU NHAT LA: " << max << " " ;
+		for(int i = 0 ; i<=n ; i++){
+			if(a[i] == max){
+				cout << " VAO NAM " << i+1920 << "  ";
+			}
+		}
+		cout << endl;
+		cout << "SO NGUOI SINH RA IT NHAT LA: " << min << " " ;
+		for(int i = 0 ; i<=n ; i++){
+			if(a[i] == min){
+				cout << "VAO NAM " << i+1920 << "  ";
+			}
+		}
+		cout << endl;
+		for(int i = 0 ; i<=n ; i++){
+			if(dem[a[i]] > 1){
+				cout << "SO LUONG NGUOI SINH RA: " << a[i] << "  ";
+				for(int j = i ; j<=n ; j++){
+					if(a[j] == a[i] && dem[a[i]] > 1){
+						cout << "VAO NAM " << j+1920 << "  " ;
+					}
+				}
+				cout << endl;
+				dem[a[i]] = 0;
+			}
+		}
+}
